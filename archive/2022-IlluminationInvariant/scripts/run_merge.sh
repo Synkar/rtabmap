bc21c3d431d96b4d7f8a9e872cb844cc64506053
LucasReis123 <reis0932@gmail.com>
2025-05-19 15:22:13 -0300

    feat(archive/2022-IlluminationInvariant/scripts/run_merge.sh): Using our detabases to create the merged map and combinations

--- archive/2022-IlluminationInvariant/scripts/run_merge.sh
+++ archive/2022-IlluminationInvariant/scripts/run_merge.sh
@@ -12,23 +12,13 @@ MIN_INLIERS=20 #20 40 60 80
 
 source rtabmap_latest.bash
 
-DATABASES="$DATA/$TYPE/map_190321-164651.db;$DATA/$TYPE/map_190321-172717.db;$DATA/$TYPE/map_190321-175428.db;$DATA/$TYPE/map_190321-182709.db;$DATA/$TYPE/map_190321-185608.db;$DATA/$TYPE/map_190321-193556.db"
+DATABASES="$DATA/$TYPE/dabi_09_58_floor0-5.2.1-2025-05-15.db;$DATA/$TYPE/dabi_12_25_floor0-5.2.1-2025-05-16.db;$DATA/$TYPE/dabi_15_12_floor0-5.2.1-2025-05-16.db;$DATA/$TYPE/dabi_17_11_floor0-5.2.1-2025-05-16.db"
 
 # To compute "Ground truth"
-rtabmap-reprocess --uwarn "$DATABASES" $DATA/$TYPE/merged_123456.db
+rtabmap-reprocess --uwarn "$DATABASES" $DATA/$TYPE/merged_1234.db
 
-cp $DATA/$TYPE/merged_123456.db $DATA/$TYPE/merged_123456_gt.db
-rtabmap-detectMoreLoopClosures -r 0.5 -i 5 $DATA/$TYPE/merged_123456_gt.db
+rtabmap-reprocess --uwarn "$DATA/$TYPE/dabi_09_58_floor0-5.2.1-2025-05-15.db;$DATA/$TYPE/dabi_17_11_floor0-5.2.1-2025-05-16.db" $DATA/$TYPE/merged_14.db
 
-rtabmap-reprocess --uwarn -gt $DATA/$TYPE/merged_123456_gt.db $DATA/$TYPE/merged_123456.db
+rtabmap-reprocess --uwarn "$DATA/$TYPE/dabi_09_58_floor0-5.2.1-2025-05-15.db;$DATA/$TYPE/dabi_15_12_floor0-5.2.1-2025-05-16.db" $DATA/$TYPE/merged_13.db
 
-rtabmap-reprocess --uwarn "$DATA/$TYPE/map_190321-164651.db;$DATA/$TYPE/map_190321-193556.db" $DATA/$TYPE/merged_16.db
-
-rtabmap-reprocess --uwarn "$DATA/$TYPE/map_190321-164651.db;$DATA/$TYPE/map_190321-175428.db;$DATA/$TYPE/map_190321-185608.db" $DATA/$TYPE/merged_135.db
-
-rtabmap-reprocess --uwarn "$DATA/$TYPE/map_190321-172717.db;$DATA/$TYPE/map_190321-182709.db;$DATA/$TYPE/map_190321-193556.db" $DATA/$TYPE/merged_246.db
-
-# Reduced graph
-rtabmap-reprocess --uwarn -gt --Mem/ReduceGraph true --Vis/MinInliers $MIN_INLIERS $DATA/$TYPE/merged_123456_gt.db $DATA/$TYPE/merged_123456_reduced.db
-
-
+rtabmap-reprocess --uwarn "$DATA/$TYPE/dabi_12_25_floor0-5.2.1-2025-05-16.db;$DATA/$TYPE/dabi_17_11_floor0-5.2.1-2025-05-16.db" $DATA/$TYPE/merged_24.db
